e3f5d6a87911fa048b813216227cc5e5191ee1b4
maju6406 <beersy@gmail.com>
2018-03-23 16:00:49 -0700

    Adding logic

--- tasks/decrypt.sh
+++ tasks/decrypt.sh
@@ -3,3 +3,4 @@ if puppet config print server | grep -v -q `hostname`; then
   echo "This task can only be run on the master node."; 
   exit 1
 fi
+cd /etc/puppetlabs/puppet/;/opt/puppetlabs/puppet/bin/eyaml decrypt -s '$PT_data'
--- tasks/init.sh
+++ tasks/init.sh
@@ -3,3 +3,16 @@ if puppet config print server | grep -v -q `hostname`; then
   echo "This task can only be run on the master node."; 
   exit 1
 fi
+/opt/puppetlabs/puppet/bin/puppet resource package hiera-eyaml ensure=present provider=puppet_gem
+/bin/tee -a newfile <<EOF
+  - name: "Eyaml hierarchy"
+    lookup_key: eyaml_lookup_key # eyaml backend
+    paths:
+      - "common.eyaml"
+      - $PT_path
+    options:
+        pkcs7_private_key: "/etc/puppetlabs/puppet/keys/private_key.pkcs7.pem"
+        pkcs7_public_key: "/etc/puppetlabs/puppet/keys/public_key.pkcs7.pem"
+EOF
+cd /etc/puppetlabs/puppet/;/opt/puppetlabs/puppet/bin/eyaml createkeys
+/opt/puppetlabs/puppet/bin/puppet agent -t
