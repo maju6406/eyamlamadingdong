e32ca92e1c398c07dc2e2c4743b1cdc9d35d0c1d
maju6406 <beersy@gmail.com>
2018-03-26 10:28:11 -0700

    Fixing some bugs

--- tasks/init.sh
+++ tasks/init.sh
@@ -5,7 +5,7 @@ if puppet config print server | grep -v -q `hostname`; then
 fi
 
 /opt/puppetlabs/puppet/bin/puppet resource package hiera-eyaml ensure=present provider=puppet_gem
-IFS=',' read -a paths <<< "${PT_path}"
+IFS=',' read -a paths <<< "${PT_paths}"
 for i in "${!paths[@]}"
 do
   path_str+=$'      -  '
@@ -22,5 +22,5 @@ $path_str
 	pkcs7_private_key: "/etc/puppetlabs/puppet/keys/private_key.pkcs7.pem"
 	pkcs7_public_key:  "/etc/puppetlabs/puppet/keys/public_key.pkcs7.pem"
 EOF
-cd /etc/puppetlabs/puppet/;/opt/puppetlabs/puppet/bin/eyaml createkeys
+cd /etc/puppetlabs/puppet/;/opt/puppetlabs/puppet/bin/eyaml createkeys | yes
 #/opt/puppetlabs/puppet/bin/puppet agent -t
