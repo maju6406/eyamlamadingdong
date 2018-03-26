514c74c24ac76e7999db8359b129463b5bd30df1
maju6406 <beersy@gmail.com>
2018-03-26 16:30:59 -0700

    Adding puppetserver reload

--- tasks/install.sh
+++ tasks/install.sh
@@ -35,5 +35,5 @@ echo "Finished! Congrats!"
 if [ "$PT_configure_global_hiera" = "false" ] ; then
   echo "Manual step: You will need to manually update your hiera.yaml files."
 fi
-#kill -HUP `pgrep -f puppet-server`
+service puppetserver reload 
 echo "Find out more about eyaml here: https://puppet.com/blog/encrypt-your-data-using-hiera-eyaml"
