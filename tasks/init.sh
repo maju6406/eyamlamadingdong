7df610ab8a46db5eb4261bfcb23fce7cccc59bce
maju6406 <beersy@gmail.com>
2018-03-26 13:07:08 -0700

    Using ruby code to update hiera file

--- tasks/init.sh
+++ tasks/init.sh
@@ -12,23 +12,22 @@ mv /etc/puppetlabs/puppet/keys/public_key.pkcs7.pem "/etc/puppetlabs/puppet/keys
 cd /etc/puppetlabs/puppet/;/opt/puppetlabs/puppet/bin/eyaml createkeys
 # Update hiera.yaml
 IFS=',' read -a paths <<< "${PT_paths}"
+path_str='["common.eyaml"'
 for i in "${!paths[@]}"
 do
-  path_str+=$'      -  '
-  path_str+=$"\"${paths[i]}\""
-  path_str+=$'\n'
+  path_str+=$",\"${paths[i]}\""
 done
+path_str+="]"
 today=`date +%Y-%m-%dT%H:%M:%S%z` 
 #make backup copy of existing hiera.yaml
 cp /etc/puppetlabs/puppet/hiera.yaml "/etc/puppetlabs/puppet/hiera.yaml.$today"
-/bin/tee -a /etc/puppetlabs/puppet/hiera.yaml <<EOF
-  - name: "Eyaml hierarchy"
-    lookup_key: eyaml_lookup_key # eyaml backend
-    paths:
-      - "common.eyaml"
-$path_str
-    options:
-      pkcs7_private_key: "/etc/puppetlabs/puppet/keys/private_key.pkcs7.pem"
-      pkcs7_public_key:  "/etc/puppetlabs/puppet/keys/public_key.pkcs7.pem"
+/bin/tee /tmp/hiera_helper.rb <<EOF
+require 'yaml'; 
+hiera =YAML.load_file('/etc/puppetlabs/puppet/hiera.yaml');
+hiera['hierarchy'].push({"name"=>"Eyaml hierarchy", "lookup_key"=>"eyaml_lookup_key", "paths"=>$path_str, "options"=>{"pkcs7_private_key"=>"/etc/puppetlabs/puppet/keys/private_key.pkcs7.pem", "pkcs7_public_key"=>"/etc/puppetlabs/puppet/keys/public_key.pkcs7.pem"}});
+output = YAML.dump hiera
+File.write('/etc/puppetlabs/puppet/hiera.yaml', output)
 EOF
+chmod a+x /tmp/hiera_helper.rb
+/opt/puppetlabs/puppet/bin/ruby /tmp/hiera_helper.rb
 #/opt/puppetlabs/puppet/bin/puppet agent -t
