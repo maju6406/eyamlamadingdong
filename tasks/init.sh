e9e5745a0b781319151518febcee02f1dfab370f
maju6406 <beersy@gmail.com>
2018-03-26 10:58:08 -0700

    Making backups

--- tasks/init.sh
+++ tasks/init.sh
@@ -4,7 +4,13 @@ if puppet config print server | grep -v -q `hostname`; then
   exit 1
 fi
 
+#install eyaml gem
 /opt/puppetlabs/puppet/bin/puppet resource package hiera-eyaml ensure=present provider=puppet_gem
+#make backup copies of existing keys
+mv /etc/puppetlabs/puppet/keys/private_key.pkcs7.pem "/etc/puppetlabs/puppet/keys/private_key.pkcs7.pem.$today"
+mv /etc/puppetlabs/puppet/keys/public_key.pkcs7.pem "/etc/puppetlabs/puppet/keys/public_key.pkcs7.pem.$today"
+cd /etc/puppetlabs/puppet/;/opt/puppetlabs/puppet/bin/eyaml createkeys
+# Update hiera.yaml
 IFS=',' read -a paths <<< "${PT_paths}"
 for i in "${!paths[@]}"
 do
@@ -12,6 +18,9 @@ do
   path_str+=$"\"${paths[i]}\""
   path_str+=$'\n'
 done
+today=`date +%Y-%m-%dT%H:%M:%S%z` 
+#make backup copy of existing hiera.yaml
+cp /etc/puppetlabs/puppet/hiera.yaml "/etc/puppetlabs/puppet/hiera.yaml.$today"
 /bin/tee -a /etc/puppetlabs/puppet/hiera.yaml <<EOF
   - name: "Eyaml hierarchy"
     lookup_key: eyaml_lookup_key # eyaml backend
@@ -22,5 +31,4 @@ $path_str
 	pkcs7_private_key: "/etc/puppetlabs/puppet/keys/private_key.pkcs7.pem"
 	pkcs7_public_key:  "/etc/puppetlabs/puppet/keys/public_key.pkcs7.pem"
 EOF
-cd /etc/puppetlabs/puppet/;/opt/puppetlabs/puppet/bin/eyaml createkeys | yes
 #/opt/puppetlabs/puppet/bin/puppet agent -t
